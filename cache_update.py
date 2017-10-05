1a6ff9442f4c0f50ef4c735e2d5bdf3975616520
Sean <sean@tox.im>
2017-10-05 02:51:19 -0700

    Modernize cache update, add a gitignore

--- cache_update.py
+++ cache_update.py
@@ -1,25 +1,22 @@
 import ConfigParser
-import cloudpickle
 import anydbm
 import os
 
-try:
-    import cPickle as pickle
-except:
-    import pickle
+import dill as pickle
 
 print "Cache updater for <1.3.2 to 1.4.0+"
-
 
 cfile = open(os.path.join('.', 'kgb.conf'), 'r+b')
 config = ConfigParser.ConfigParser()
 config.readfp(cfile)
 
-def config_get(module, item, default=False):    #Stolen right from IC
+
+def config_get(module, item, default=False):  # Stolen right from IC
     if config.has_option(module, item):
         return config.get(module, item)
     else:
         return default
+
 
 print "Reading config file to find cache name"
 
@@ -28,9 +25,7 @@ try:
 except:
     cache_name = ".cache"
 
-
 cache_name = os.path.join('.', cache_name)
-
 
 if os.path.isfile(cache_name):
     try:
@@ -55,12 +50,11 @@ cache_fd.close()
 os.rename(cache_name, '.cache.old')
 print "Old cache renamed to .cache.old"
 
-
-cache_new = anydbm.open('.new_cache', 'c')
+cache_new = anydbm.open('.cache', 'c')
 
 for item in cache_old:
-    cache_new[item] = cloudpickle.dumps(cache_old[item])
+    cache_new[item] = pickle.dumps(cache_old[item])
 
 cache_new.close()
 
-print "New cache saved to .cache.db!"
+print "New cache saved to .cache!"
