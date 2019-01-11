c4ecc3ee7923ddfb70583182fac2c5ee2f5d5564
Sean Qureshi <squreshi77@icloud.com>
2019-01-11 12:58:47 -0800

    sql fix

--- database.sql
+++ database.sql
@@ -3,9 +3,17 @@ create table command (
     response text
 );
 
+create table users (
+    username text unique,
+    nickname text unique,
+    ident text,
+    hostmask text unique
+);
+
 create table profile (
     username text unique,
     nickname text unique,
+    password text,
     ident text,
     hostmask text unique,
     loc_lat float,
